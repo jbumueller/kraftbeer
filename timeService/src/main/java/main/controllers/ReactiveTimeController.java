798e651be91f5ec4e401444ca9409d040c995abd
Jan Carsten <j.c.lohmueller@googlemail.com>
2016-04-26 20:34:37 +0200

    introduced reactive time controller using rxjava

---
+++ timeService/src/main/java/main/controllers/ReactiveTimeController.java
@@ -0,0 +1,23 @@
+package main.controllers;
+
+import org.joda.time.DateTime;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import rx.Observable;
+
+@RestController
+public class ReactiveTimeController {
+	@RequestMapping("/timereact")
+	public DeferredResult<String> getTimeAsync() {
+		Observable<String> observable = this.getTime();
+		DeferredResult<String> deffered = new DeferredResult<>();
+		observable.subscribe(result -> deffered.setResult(result), error -> deffered.setErrorResult(error));
+		return deffered;
+	}
+
+	public Observable<String> getTime() {
+		return Observable.just(DateTime.now().toString());
+	}
+}
