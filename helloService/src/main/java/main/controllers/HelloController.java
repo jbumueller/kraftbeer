57ecea5c48b9518f748ad0b01386673611eb7c96
Jens Barthel <jens.barthel@s24.com>
2016-04-26 22:28:39 +0100

    Add time service call to hello service

--- helloService/src/main/java/main/controllers/HelloController.java
+++ helloService/src/main/java/main/controllers/HelloController.java
@@ -2,12 +2,15 @@ package main.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
-
     @RequestMapping("/")
     public String index(){
-    	return "Hello from hell";
+    	RestTemplate restTemplate = new RestTemplate();
+        String timeAsString = restTemplate.getForObject("http://192.168.99.100:9090/timereact", String.class);
+    	
+    	return "Hello from hell at: " + timeAsString;
     }
 }
