26718370b285daf5d8b52484e0b365f607c767e6
Jens Barthel <jens.barthel@gmail.com>
2016-05-19 22:26:23 +0200

    Change pom group ids to io.kraftbike

---
+++ timeService/src/main/java/main/controllers/BikeController.java
@@ -0,0 +1,13 @@
+package main.controllers;
+
+import java.util.UUID;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BikeController {
+	
+	public Bike get(UUID id) {
+		
+	}
+}
