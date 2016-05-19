c4152589b113f29e0b41e2aaf9c5d36d86a464b7
Jens Barthel <jens.barthel@gmail.com>
2016-05-19 22:38:22 +0200

    Create BikeController and fix dependencies

--- timeService/src/main/java/main/controllers/BikeController.java
+++ timeService/src/main/java/main/controllers/BikeController.java
@@ -4,10 +4,12 @@ import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import kraftbike.domain.model.Bike;
+
 @RestController
 public class BikeController {
 	
 	public Bike get(UUID id) {
-		
+		return null;
 	}
 }
