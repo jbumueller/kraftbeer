bf52d76f2d660839389ebc45945a6d812415997a
Jens Barthel <jens.barthel@gmail.com>
2016-05-20 00:37:37 +0200

    Add autowiring of BikeService to controller

--- timeService/src/main/java/main/controllers/BikeController.java
+++ timeService/src/main/java/main/controllers/BikeController.java
@@ -2,14 +2,23 @@ package main.controllers;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import kraftbike.domain.model.Bike;
+import kraftbike.domain.service.BikeService;
 
 @RestController
 public class BikeController {
+
+	@Autowired
+	private BikeService bikeService;
 	
-	public Bike get(UUID id) {
-		return null;
+	@RequestMapping( path="/api/bikes/{bikeId}", method=RequestMethod.GET)
+	public Bike get(@PathVariable UUID bikeId) {
+		return bikeService.get(bikeId);
 	}
 }
